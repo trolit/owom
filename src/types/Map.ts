a8ddbf6cd53910dbfece8cfa763efb4c553aae51
p4w31 !d21k0w541 <iddziku@gmail.com>
2024-05-27 19:13:23 +0200

    Update Map.ts

--- src/types/Map.ts
+++ src/types/Map.ts
@@ -2,11 +2,11 @@ import { OwomMapper } from "@owom";
 import { Constructor } from "./Constructor";
 
 export type MapFuncWithoutDi<T> = <Y extends OwomMapper<T>>(
-  Mapper: Constructor<T, Y>
+  Mapper: Constructor<T, Y>,
 ) => Y;
 
 export type MapManyFuncWithoutDi<T> = <Y extends OwomMapper<T>>(
-  Mapper: Constructor<T, Y>
+  Mapper: Constructor<T, Y>,
 ) => Y[];
 
 export type MapFuncWithDi<Y> = (token: string) => Y;
